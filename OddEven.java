a3a2a160c2b44745ca34352a5718cc530bf42cb9
kiruba2196 <kiruba2196@gmail.com>
2017-09-01 14:41:12 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class OddEven {
+	public static void main(String arg[])
+    {
+		Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        if(k%2==0)
+        System.out.println("Given no is Even");
+        else
+        System.out.println("Given no is odd");
+    }
+}
