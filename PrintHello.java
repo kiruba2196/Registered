a88ad9029103eb52e8ff5fbab90ff33856188468
kiruba2196 <kiruba2196@gmail.com>
2017-09-01 15:11:10 +0530

    Create PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,11 @@
+    import java.util.Scanner;
+    public class PrintHello
+    {
+        public static void main(String args[])
+        {
+        	Scanner sc=new Scanner(System.in);
+        	for(int i=0;i<5;i++)
+        		System.out.println("Hello");
+
+        }
+    }
