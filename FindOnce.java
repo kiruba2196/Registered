087a492a9fcca57c3ec985c37bc9533e6c07fd5a
kiruba2196 <kiruba2196@gmail.com>
2017-09-01 14:04:58 +0530

    Create FindOnce.java

---
+++ FindOnce.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class FindOnce {
+	public static void main(String arg[])
+    {
+        int count;
+		Scanner sc=new Scanner(System.in);
+        String st=sc.nextLine();
+        String[] arr=st.split(" ");
+        for(int i=0;i<arr.length;i++)
+        {
+        	count=0;
+        	for(int j=0;j<arr.length;j++)
+        	{
+        	if(arr[i].equals(arr[j]) && i!=j)
+        	{
+        		count++;
+        	}
+        	}
+        	if(count==0)
+        		System.out.println(arr[i]);
+        }
+    }
+
+}
